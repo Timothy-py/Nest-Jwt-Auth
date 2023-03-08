dcc711b4f4ab159a6dfc3f3cd3cb5867857d642e
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-08 23:49:01 +0100

    add class validator to dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,8 +8,8 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('local/signup')
-    signupLocal(){
-        this.authService.signupLocal()
+    signupLocal(@Body() dto:AuthDto){
+        this.authService.signupLocal(dto)
     }
 
     @Post('local/signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma:PrismaService){}
 
-    async signupLocal(){}
+    async signupLocal(dto:AuthDto){
+
+    }
 
     async signinLocal(){}
 
