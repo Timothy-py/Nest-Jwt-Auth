7ec5d88a99e47a5ec2d27361da2feb89b216d4cf
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-10 23:30:14 +0100

    added return type to signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt'
+import { Tokens } from './types';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma:PrismaService){}
 
-    async signupLocal(dto:AuthDto){
+    hashData(data: string){
+        return bcrypt.hash(data, 10)
+    }
 
+    async signupLocal(dto:AuthDto): Promise<Tokens>{
+        const hash = await this.hashData(dto.password)
+
+        const newUser = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash: hash
+            }
+        })
     }
 
     async signinLocal(){}
