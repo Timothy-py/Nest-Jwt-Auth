210f2e6504f02340e240bd5fbdb465c311c0992b
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-11 23:22:25 +0100

    feat<authenticaton>: write function for getting access and refresh tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,41 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt'
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma:PrismaService){}
+    constructor(
+        private prisma:PrismaService,
+        private jwtService: JwtService
+    ){}
 
     hashData(data: string){
         return bcrypt.hash(data, 10)
+    }
+
+    async getTokens(userId: number, email: string): Promise<Tokens>{
+        const [at, rt] = await Promise.all([
+            this.jwtService.signAsync({
+                sub: userId,
+                email: email
+            }, {
+                secret: 'at-secret',
+                expiresIn: 60 * 15
+            }),
+            this.jwtService.signAsync({
+                sub: userId,
+                email: email
+            }, {
+                secret: 'rt-secret',
+                expiresIn: 60 * 60 * 24 * 7
+            })
+        ]);
+
+        return {
+            access_token: at,
+            refresh_token: rt
+        }
     }
 
     async signupLocal(dto:AuthDto): Promise<Tokens>{
