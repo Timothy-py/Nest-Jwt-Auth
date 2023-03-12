fea1f599847fee7eaa6f37a75182d03a58f9a267
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-12 23:00:38 +0100

    fix<authentication>: add default status codes to api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
@@ -9,21 +9,25 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('local/signup')
+    @HttpCode(201)
     signupLocal(@Body() dto:AuthDto): Promise<Tokens>{
         return this.authService.signupLocal(dto)
     }
 
     @Post('local/signin')
+    @HttpCode(200)
     signinLocal(@Body() dto:AuthDto): Promise<Tokens>{
         return this.authService.signinLocal(dto)
     }
 
     @Post('logout')
+    @HttpCode(200)
     logout(){
         this.authService.logout()
     }
 
     @Post('refresh')
+    @HttpCode(200)
     refreshTokens(){
         this.authService.refreshTokens()
     }
