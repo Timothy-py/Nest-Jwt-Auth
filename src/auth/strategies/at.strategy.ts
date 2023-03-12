209876337ea3f371ac02dbbdbd4c33fc0a217af1
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-12 23:12:55 +0100

    created jwt payload type

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -1,6 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { type } from "os";
 import { ExtractJwt, Strategy } from "passport-jwt";
+
+type JwtPayload = {
+    sub: string,
+    email: string
+}
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt'){
@@ -12,7 +18,7 @@ export class AtStrategy extends PassportStrategy(Strategy, 'jwt'){
         })
     }
 
-    validate(payload: any){
+    validate(payload: JwtPayload){
         return payload;
     }
 }
