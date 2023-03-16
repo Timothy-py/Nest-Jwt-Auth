da73144f845e6de7e03acbf6082e54404c12863c
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-16 01:43:30 +0100

    implement canActivate func on AT guard for detecting Public route

--- src/auth/guard/at.guard.ts
+++ src/auth/guard/at.guard.ts
@@ -1,13 +1,22 @@
-import { ExecutionContext } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
+@Injectable()
 export class AtGuard extends AuthGuard('jwt'){
-    constructor(){
+    constructor(private reflector: Reflector){
         super()
     }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        return true
+        const isPublic = this.reflector.getAllAndOverride('isPublic', [
+            context.getHandler(),
+            context.getClass()
+        ]);
+
+        if(isPublic) return true;
+
+        return super.canActivate(context)
     }
 }
